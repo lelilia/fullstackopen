50d782bcccc59257aa8e629430fce8e7668ca6e2
Leli <lelilia@posteo.de>
2020-08-06 01:39:25 +0200

    finished part 1d

--- part1/course_content/src/index.js
+++ part1/course_content/src/index.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) =>(
-  <div>
-    <p>Hello, {props.name}! You are {props.age} years old.</p>
-  </div>
+const Button = (props) => (
+  <button onClick = {props.handeClick}>
+    {props.text}
+  </button>
 )
 
-const App = () => {
-  const name = "Peter"
-  const age = 10
+const Display = (props) => (
+  <div>{props.value}</div>
+)
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    setValue(newValue)
+  }
+
+  const hello = (who) => () => console.log("hello", who)
+  
+
+  const handeClick = () => {
+    console.log('clicked the button')
+    setValue(0)
+  }
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name = "Leli" age = {26+8}/>
-      <Hello name = {name} age = {age}/>
-      <Hello />
+      <Display value = {value} />
+
+      <Button handeClick = {() => setToValue(1000)} text = "1000" />
+      <Button handeClick = {() => setToValue(0)} text = "0" />
+      <Button handeClick = {() => setToValue(value + 1)} text = "+1" />
+     
     </div>
   )
 }
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'))
