94dd49b138c644100dc5d421af25360810d832c3
Leli <lelilia@posteo.de>
2020-08-06 03:12:12 +0200

    part 1.14

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ handeClick, text }) => (
+  <button onClick = {handeClick}>{text}</button>
+)
+
+const Heading = ({ text }) => <h1>{text}</h1>
+
+const Display = ({ text, points }) => (
+  <>
+  <p>{text}</p>
+  <p>has {points} votes</p>
+  </>
+  )
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0)
+
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+  
+  const updateVotes = (points, selected) => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
+  }
+
+  const maxVotes = Math.max(...points)
+  const maxIndex = points.indexOf(maxVotes)
+  
+  return (
+    <div>
+      <Heading text = "Anecdote of the day" />
+      <Display text = {anecdotes[selected]} points = {points[selected]} />
+      <Button handeClick = {() => updateVotes(points, selected)} text = "vote" />
+      <Button handeClick = {() => setSelected(Math.floor(Math.random() * anecdotes.length))} text = "next anecdote" />
+      <Heading text = "Anecdote with most votes" />
+      <Display text = {anecdotes[maxIndex]} points = {maxVotes} />
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes = {anecdotes} />,
+  document.getElementById("root")
+)
