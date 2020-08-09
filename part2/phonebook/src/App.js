476f6d86d458695f94d561c9e304191cc17f97dd
Leli <lelilia@posteo.de>
2020-08-09 03:36:39 +0200

    part 2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
-import NewEntryForm  from './components/NewEntryForm '
+import NewEntryForm from './components/NewEntryForm '
 import Entries from './components/Entries'
 import Header from './components/Header'
 import entryServices from './services/entries'
@@ -8,10 +8,10 @@ import entryServices from './services/entries'
 
 
 const App = () => {
-  const [ entries, setEntries ] = useState([])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filter, setFilter ] = useState('')
+  const [entries, setEntries] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     entryServices
@@ -23,9 +23,23 @@ const App = () => {
 
   const addEntry = (event) => {
     event.preventDefault()
+    const filteredEntries = entries.filter(entry => entry.name === newName)
+    if (filteredEntries.length !== 0) {
+      
+      const entry = filteredEntries[0]
+      const id = entry.id
+      const confirmMessage = `${entry.name} is already added to the phonebook, replace the old number with a new one?`
 
-    if (entries.filter(entry => entry.name === newName).length !== 0) {
-      alert(`${newName} is already added to the phonebook`) 
+      if (window.confirm(confirmMessage)) {
+        const changedEntry = { ...entry, number: newNumber }
+
+        entryServices
+          .update(id, changedEntry)
+          .then(returnedEntry => {
+            setEntries(entries.map(entry => entry.id !== id ? entry : returnedEntry))
+          })
+      }
+      
     }
     else {
       const newEntryObject = {
@@ -37,52 +51,53 @@ const App = () => {
         .create(newEntryObject)
         .then(returnedEntry => {
           setEntries(entries.concat(returnedEntry))
-          setNewNumber('')
-          setNewName('')
+
         })
     }
+    setNewNumber('')
+    setNewName('')
   }
 
   const deleteEntry = entry => {
-    if (window.confirm(`Delete ${entry.name}?`)){
+    if (window.confirm(`Delete ${entry.name}?`)) {
       const id = entry.id
       entryServices
-      .deleteEntry(entry.id)
-      .then(() => {
-        setEntries(entries.filter(entry => entry.id !== id))
-      })
+        .deleteEntry(entry.id)
+        .then(() => {
+          setEntries(entries.filter(entry => entry.id !== id))
+        })
     }
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
-  
+
   const handleFilterChange = (event) => setFilter(event.target.value.toLowerCase())
 
-  const entriesToShow = filter.length === 0 
-    ? entries 
+  const entriesToShow = filter.length === 0
+    ? entries
     : entries.filter(entry => entry.name.toLowerCase().includes(filter))
 
   return (
     <div>
-      <button onClick = {deleteEntry} >delete</button>
-      <Header text = "Phonebook"/>
-      <Filter filter = {filter} onChange = { handleFilterChange } />
-      <Header text = "add a new" />
-      <NewEntryForm 
-        addPerson = { addEntry } 
-        nameChange = { handleNameChange } 
-        numberChange = { handleNumberChange }
-        newName = { newName }
-        newNumber = { newNumber }
+      <button onClick={deleteEntry} >delete</button>
+      <Header text="Phonebook" />
+      <Filter filter={filter} onChange={handleFilterChange} />
+      <Header text="add a new" />
+      <NewEntryForm
+        addPerson={addEntry}
+        nameChange={handleNameChange}
+        numberChange={handleNumberChange}
+        newName={newName}
+        newNumber={newNumber}
       />
-      <Header text = "Numbers" />
-      <Entries 
-        entries = { entriesToShow } 
-        deleteButton = {deleteEntry}
+      <Header text="Numbers" />
+      <Entries
+        entries={entriesToShow}
+        deleteButton={deleteEntry}
       />
-      
+
     </div>
   )
 }
--- part2/phonebook/src/services/entries.js
+++ part2/phonebook/src/services/entries.js
@@ -17,4 +17,9 @@ const deleteEntry = id => {
   return axios.delete(url)
 }
 
-export default { getAll, create, deleteEntry}
+const update = (id, newObject) => {
+  const request = axios.put(`${baseURL}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create, deleteEntry, update }
