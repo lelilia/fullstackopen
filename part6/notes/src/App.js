0b06129c44ac7456f305d1387349aa642f3d2a57
Leli <lelilia@posteo.de>
2020-09-19 00:34:44 +0200

    starting part 7

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
+  
   useEffect(() => {
     dispatch(initializeNotes())
   }, [dispatch])
---
+++ part7/notes/src/index.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link, useParams, useHistory, Redirect, useRouteMatch
+} from "react-router-dom"
+
+
+
+const Login = (props) => {
+  const history = useHistory()
+
+  const onSubmit = (event) => {
+    event.prevetDefault()
+    props.onLogin('mlukkai')
+    history.push('/')
+  }
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input></input>
+        </div>
+        <div>
+          password: <input type='password' />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+const Home = () => (
+  <div> <h2>TKTL notes app</h2> </div>
+)
+
+const Note = ({ note }) => {
+  const id = useParams().id
+  
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
+    </div>
+  )
+}
+
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
+
+const App = () => {
+  const [notes, setNotes] = useState([{ content: 'test', user: 'icw', id: 8, important: false }])
+  const [user, setUser] = useState(null)
+  const login = (user) => {
+    setUser(user)
+  }
+  const padding = {
+    padding: 5
+  }
+
+  const match = useRouteMatch('/notes/:id')
+  const note = match 
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
+
+  console.log(note)
+  return (
+    <div>
+      <div>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </div>
+
+      <Switch>
+        <Route path="/notes/:id">
+          <Note note={note} />
+        </Route>
+        <Route path="/notes">
+          <Notes notes={notes} />
+        </Route>
+        <Route path="/users" render={() =>
+          user ? <Users /> : <Redirect to="/login" />
+        } />
+        <Route path="/login">
+          <Login onLogin={login} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Switch>
+      <div>
+        <br />
+        <i>Note app, Department of Computer Science 2020</i>
+      </div>
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
