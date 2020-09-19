6d482f17d3256aa597dd244cb4dbc44fe851e984
Leli <lelilia@posteo.de>
2020-09-19 22:54:47 +0200

    worked now for reasons

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -4,8 +4,19 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
-  }
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    ],
+  },
 }
 
 module.exports = config
