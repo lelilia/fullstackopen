64726d033458644a1d31e5a6361c80bd13e543c5
Leli <lelilia@posteo.de>
2020-09-19 19:28:03 +0200

    finished with bootstrap

--- part7/notes/src/index.js
+++ part7/notes/src/index.js
@@ -4,7 +4,7 @@ import {
   BrowserRouter as Router,
   Switch, Route, Link, useHistory, Redirect, useRouteMatch
 } from "react-router-dom"
-
+import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
 
 
 const Login = (props) => {
@@ -19,25 +19,37 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input></input>
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type='submit'>login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+          />
+          <Form.Label>password:</Form.Label>
+          <Form.Control
+            type="password"
+          />
+          <Button variant="primary" type="submit">
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
 
 const Home = () => (
-  <div> <h2>TKTL notes app</h2> </div>
+  <div>
+    <h2>TKTL notes app</h2>
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  </div>
 )
 
 const Note = ({ note }) => {
-  
+
   return (
     <div>
       <h2>{note.content}</h2>
@@ -47,16 +59,25 @@ const Note = ({ note }) => {
   )
 }
 
-const Notes = ({ notes }) => (
+const Notes = (props) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {props.notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
@@ -67,30 +88,54 @@ const Users = () => (
 const App = () => {
   const [notes, setNotes] = useState([{ content: 'test', user: 'icw', id: 8, important: false }])
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
+
   }
   const padding = {
     padding: 5
   }
 
   const match = useRouteMatch('/notes/:id')
-  const note = match 
+  const note = match
     ? notes.find(note => note.id === Number(match.params.id))
     : null
 
   console.log(note)
   return (
-    <div>
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/notes">notes</Link>
-        <Link style={padding} to="/users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
-        }
-      </div>
+    <div className="container">
+      {(message &&
+        <Alert variant="success">
+          {message}
+        </Alert>
+      )}
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/notes">notes</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/users">users</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user
+                ? <em>{user} logged in</em>
+                : <Link to="/login">login</Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Switch>
         <Route path="/notes/:id">
