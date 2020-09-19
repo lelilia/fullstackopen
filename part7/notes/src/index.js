3b53d2c175b79d4e2a383f154e344077d427ec0e
Leli <lelilia@posteo.de>
2020-09-19 19:49:13 +0200

    finished with material ui

--- part7/notes/src/index.js
+++ part7/notes/src/index.js
@@ -4,37 +4,49 @@ import {
   BrowserRouter as Router,
   Switch, Route, Link, useHistory, Redirect, useRouteMatch
 } from "react-router-dom"
-import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
+
+import {
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
+  AppBar,
+  Toolbar,
+  IconButton
+} from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 
 
 const Login = (props) => {
   const history = useHistory()
 
   const onSubmit = (event) => {
-    event.prevetDefault()
-    props.onLogin('mlukkai')
+    event.preventDefault()
+    props.onLogin('mluukkai')
     history.push('/')
   }
 
   return (
     <div>
       <h2>login</h2>
-      <Form onSubmit={onSubmit}>
-        <Form.Group>
-          <Form.Label>username:</Form.Label>
-          <Form.Control
-            type="text"
-            name="username"
-          />
-          <Form.Label>password:</Form.Label>
-          <Form.Control
-            type="password"
-          />
-          <Button variant="primary" type="submit">
+      <form onSubmit={onSubmit}>
+        <div>
+          <TextField label="username" />
+        </div>
+        <div>
+          <TextField label="password" type='password' />
+        </div>
+        <div>
+          <Button variant="contained" color="primary" type="submit">
             login
           </Button>
-        </Form.Group>
-      </Form>
+        </div>
+      </form>
     </div>
   )
 }
@@ -59,25 +71,26 @@ const Note = ({ note }) => {
   )
 }
 
-const Notes = (props) => (
+const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <Table striped>
-      <tbody>
-        {props.notes.map(note =>
-          <tr key={note.id}>
-            <td>
-              <Link to={`/notes/${note.id}`}>
-                {note.content}
-              </Link>
-            </td>
-            <td>
-              {note.user}
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </Table>
+
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map(note => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>
+                {note.name}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 )
 
@@ -108,35 +121,33 @@ const App = () => {
 
   console.log(note)
   return (
-    <div className="container">
-      {(message &&
-        <Alert variant="success">
-          {message}
-        </Alert>
-      )}
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/">home</Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/notes">notes</Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              <Link style={padding} to="/users">users</Link>
-            </Nav.Link>
-            <Nav.Link href="#" as="span">
-              {user
-                ? <em>{user} logged in</em>
-                : <Link to="/login">login</Link>
-              }
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
+    <Container>
+      <div>
+        {(message &&
+          <Alert severity="success">
+            {message}
+          </Alert>
+        )}
+      </div>
+      <AppBar position="static">
+  <Toolbar>
+    <Button color="inherit" component={Link} to="/">
+      home
+    </Button>
+    <Button color="inherit" component={Link} to="/notes">
+      notes
+    </Button>
+    <Button color="inherit" component={Link} to="/users">
+      users
+    </Button>   
+    {user
+      ? <em>{user} logged in</em>
+      : <Button color="inherit" component={Link} to="/login">
+          login
+        </Button>
+    }                              
+  </Toolbar>
+</AppBar>
       <Switch>
         <Route path="/notes/:id">
           <Note note={note} />
@@ -159,7 +170,7 @@ const App = () => {
         <br />
         <i>Note app, Department of Computer Science 2020</i>
       </div>
-    </div>
+    </Container>
   )
 }
 
